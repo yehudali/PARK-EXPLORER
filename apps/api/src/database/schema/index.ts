ad442d0bc7c7c0fc9a646cb6f8cb83c954d4716f
yehuda-linker <yehuda.linker@gmail.com>
2026-08-23 19:24:00 +0300

    c5: פיצול סכמה - קשרים לקובץ relations, index כברל

--- apps/api/src/database/schema/index.ts
+++ apps/api/src/database/schema/index.ts
@@ -1,43 +1,7 @@
-import { relations } from 'drizzle-orm';
-import { parks } from './parks.schema';
-import { users } from './users.schema';
-import { parkImages } from './park-images.schema';
-import { regions } from './regions.schema';
-import { cities } from './cities.schema';
+export * from './regions.schema';
+export * from './cities.schema';
+export * from './users.schema';
+export * from './parks.schema';
+export * from './park-images.schema';
 
-export const parksRelations = relations(parks, ({ one, many }) => ({
-  city: one(cities, {
-    fields: [parks.cityId],
-    references: [cities.id],
-  }),
-  creator: one(users, {
-    fields: [parks.creatorId],
-    references: [users.id],
-  }),
-  images: many(parkImages),
-}));
-
-export const citiesRelations = relations(cities, ({ one, many }) => ({
-  parks: many(parks),
-  region: one(regions, {
-    fields: [cities.regionId],
-    references: [regions.id],
-  }),
-}));
-
-export const regionsRelations = relations(regions, ({ many }) => ({
-  cities: many(cities),
-}));
-
-export const usersRelations = relations(users, ({ many }) => ({
-  parks: many(parks),
-}));
-
-export const parkImagesRelations = relations(parkImages, ({ one }) => ({
-  park: one(parks, {
-    fields: [parkImages.parkId],
-    references: [parks.id],
-  }),
-}));
-
-export { users, regions, cities, parks, parkImages };
+export * from './relations';
