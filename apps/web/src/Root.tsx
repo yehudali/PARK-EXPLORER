3bbdc58bdd4f29acde9342475ea40529e8ada30e
yehuda-linker <yehuda.linker@gmail.com>
2026-08-23 16:03:15 +0300

    fix/ format document

--- apps/web/src/Root.tsx
+++ apps/web/src/Root.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { httpBatchLink } from '@trpc/client'
-import { trpc } from './lib/trpc'
-import App from './App.tsx'
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { trpc } from "./lib/trpc";
+import App from "./App.tsx";
 
-const TRPC_URL = 'http://localhost:3000/trpc'
+const TRPC_URL = "http://localhost:3000/trpc";
 
 export default function Root() {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [httpBatchLink({ url: TRPC_URL })],
-    })
-  )
+    }),
+  );
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
@@ -20,5 +20,5 @@ export default function Root() {
         <App />
       </QueryClientProvider>
     </trpc.Provider>
-  )
+  );
 }
